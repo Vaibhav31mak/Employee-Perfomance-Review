5a0371c2a182eeb62bdb4539891b32b43e760926
Anish59312 <anish31442@gmail.com>
2025-01-26 09:53:38 +0530

    optimized create.cs for leave application

--- EPR/obj/Debug/net8.0/EPR.AssemblyInfo.cs
+++ EPR/obj/Debug/net8.0/EPR.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EPR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a2e8c0e07ab2110eb2b00dfeecff269f4ec9969")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea274260c5d9c1692e3c6e92b3e8b2e2dfdaa94")]
 [assembly: System.Reflection.AssemblyProductAttribute("EPR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EPR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
